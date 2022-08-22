46a5d6f04d2172fbadfee6dcd044db1d2311390d
jerryjohns <jerry.johns@student.moringaschool.com>
2022-08-22 21:43:02 +0300

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+
+    self.end_with? "."    
   end
 
   def question?
 
+    self.end_with? "?"    
   end
 
   def exclamation?
+    self.end_with? "!"    
 
   end
 
   def count_sentences
 
+    # puts self
+    split_sent_array = self.split(/[.!?]/)
+    # pp split_sent_array
+    split_sent_array.delete_if {|elem| elem == ""}
+    # pp split_sent_array
+    no = split_sent_array.count
+    # binding.pry
   end
 end
